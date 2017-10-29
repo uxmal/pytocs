518d7e43d7ef9754144fc8192e37bd25a9cdbfee
John Källén <johnkal@gmail.com>
2017-10-29 20:49:04 +0100

    #2: On the suggestion of @munrocket, added a more complex example to illustrate what pytocs can do.

---
+++ subjects/readme/readme.py
@@ -0,0 +1,22 @@
+class MyClass:
+    # member function calling other function
+    def calc_sum(self, x, y):
+       return self.frobulate('+', x, y)
+
+    # arithmetic and exceptions
+    def frobulate(self, op, x, y):
+        if op == '+':
+            return x + y
+        elif op == '-':
+            return x - y
+        else:
+            raise ValueError("Unexpected argument %s" % op)
+
+    # static method using for..in and enumerate, with tuple comprehension
+    def walk_list(lst):
+        for (i,strg) in lst.iterate():
+            print "index: %d strg: %s\n" % (i, strg)
+ 
+    # list comprehension
+    def apply_map(mapfn, filterfn):
+        return [mapfn(n) for n in lst if filterfn]
---
+++ subjects/readme/readme.py.cs
@@ -0,0 +1,37 @@
+
+using System;
+
+public static class readme {
+    
+    public class MyClass {
+        
+        public virtual object calc_sum(object x, object y) {
+            return this.frobulate("+", x, y);
+            // arithmetic and exceptions
+        }
+        
+        public virtual object frobulate(object op, object x, object y) {
+            if (op == "+") {
+                return x + y;
+            } else if (op == "-") {
+                return x - y;
+            } else {
+                throw ValueError(String.Format("Unexpected argument %s", op));
+                // static method using for..in and enumerate, with tuple comprehension
+            }
+        }
+        
+        public static object walk_list(object lst) {
+            foreach (var _tup_1 in lst.iterate()) {
+                var i = _tup_1.Item1;
+                var strg = _tup_1.Item2;
+                Console.WriteLine(String.Format("index: %d strg: %s\n", i, strg));
+                // list comprehension
+            }
+        }
+        
+        public static object apply_map(object mapfn, object filterfn) {
+            return lst.Where(n => filterfn).Select(n => mapfn(n));
+        }
+    }
+}
