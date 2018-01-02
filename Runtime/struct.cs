1255e5a58bdd3a0e5de657a2582e05133526b8d9
John Källén <johnkal@gmail.com>
2018-01-02 14:50:38 +0100

    Added a Runtime to pytocs.

---
+++ Runtime/struct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pytocs.runtime
+{
+    public static class @struct
+    {
+        public static object[] unpack(string format, object buffer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
