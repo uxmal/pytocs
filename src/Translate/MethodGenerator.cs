70aed9e7858bf553f8e97b37c6fee8080a8532a3
John Källén <johnkal@gmail.com>
2018-10-15 21:06:14 +0200

    Update license texts

--- src/Translate/MethodGenerator.cs
+++ src/Translate/MethodGenerator.cs
@@ -1,5 +1,5 @@
 ﻿    #region License
-//  Copyright 2015 John Källén
+//  Copyright 2015-2018 John Källén
 // 
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
