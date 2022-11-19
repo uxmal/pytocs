d3d0f3c23f2d9217ae73faafbafd9d1b3c71e9d6
John Källén <johnkal@gmail.com>
2022-11-19 14:52:58 +0100

    Fix release scripts

--- release.bat
+++ release.bat
@@ -9,7 +9,7 @@ path=%path%;%windir%\Microsoft.net\Framework\v4.0.30319
 rem go to current folder
 cd %~dp0
 
-msbuild release.msproj
+msbuild release.msproj /v:m
 
 rem To change the output folder, use the following parameter: /p:BuildDir=C:\BuildTest
 
